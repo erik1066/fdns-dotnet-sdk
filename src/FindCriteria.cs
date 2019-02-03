7d51a7be1eceecab680dc31ea71a7123b8a02006
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-02-03 14:18:12 -0500

    Added a FindCriteria constructor

--- src/FindCriteria.cs
+++ src/FindCriteria.cs
@@ -33,5 +33,47 @@ namespace Foundation.Sdk
         /// </summary>
         /// <returns>1 or -1</returns>
         public int GetNumericSortDirection() => SortDirection == ListSortDirection.Descending ? 1 : -1;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="start">The index within the find results at which to start filtering</param>
+        /// <param name="limit">The number of items within the find results to limit the result set to</param>
+        /// <param name="sortFieldName">The Json property name of the object on which to sort</param>
+        /// <param name="sortDirection">The sort direction</param>
+        public FindCriteria(int start, int limit, string sortFieldName, ListSortDirection sortDirection)
+        {
+            Start = start;
+            Limit = limit;
+            SortFieldName = sortFieldName;
+            SortDirection = sortDirection;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="start">The index within the find results at which to start filtering</param>
+        /// <param name="limit">The number of items within the find results to limit the result set to</param>
+        /// <param name="sortFieldName">The Json property name of the object on which to sort</param>
+        /// <param name="sortDirection">The sort direction; 1 for descending and -1 for ascending</param>
+        public FindCriteria(int start, int limit, string sortFieldName, int sortDirection)
+        {
+            Start = start;
+            Limit = limit;
+            SortFieldName = sortFieldName;
+
+            if (sortDirection == -1)
+            {
+                SortDirection = ListSortDirection.Ascending;
+            }
+            else if (sortDirection == 1)
+            {
+                SortDirection = ListSortDirection.Descending;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(sortDirection));
+            }
+        }
     }
 }
